9781bd8dff8890fb90730d4569648a3145807ed3
weichieh421 <weichieh400221@gmail.com>
2025-06-07 18:33:18 +0800

    更新設定與部署腳本

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/water_management/', 
   plugins: [react()],
-})
+});
